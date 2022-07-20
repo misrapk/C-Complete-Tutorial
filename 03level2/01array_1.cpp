899dd65d9836ff3d6c34590cf8e39b04d3d18c70
Peeyush <pkmisra1999@gmail.com>
2022-07-20 19:36:42 +0530

    array part 1 added

---
+++ 03level2/01array_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    const int innings[4]{20, 80, 100, 120};
+    // cout<<innings[0]<<endl;
+    // cout<<innings[1]<<endl;
+    // cout<<innings[2]<<endl;
+    // cout<<innings[3]<<endl;
+
+    for(int i{0}; i<4; i++){
+        cout<<innings[i]<<endl;
+    }
+
+}
